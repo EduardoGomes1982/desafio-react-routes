5104d2f8816928768ffca59fd4434eca9ae57e23
EduardoGomes1982 <edu1982talk@gmail.com>
2022-12-15 20:46:56 -0300

    Created Routes Structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProductCatalog from "./components/MainBody";
+import Home from "./routes/Home";
+
 export default function App() {
   return (
-    <h1>Bem vindos!!!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="/products" element={<ProductCatalog />}>
+            <Route index element={<ProductList />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
