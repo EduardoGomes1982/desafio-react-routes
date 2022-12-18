aa75a02829c2b890febf38b95a9955d843623170
EduardoGomes1982 <edu1982talk@gmail.com>
2022-12-17 21:59:31 -0300

    Page of Home Route completed

--- frontend/src/components/HomeIndex/index.tsx
+++ frontend/src/components/HomeIndex/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css"
 
 export default function HomeIndex() {
     return (
-        <section id="home-index-section">
+        <section id="home-index-section" className="container">
             <h2>Bem-vindos!!!</h2>
         </section>
     );
